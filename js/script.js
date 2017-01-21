2a3a2f136f0006d80eab117807dd095b32f816c9
pawell67 <pawellwankiewicz@gmail.com>
2017-01-21 19:44:08 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+jQuery(function($){
+//scroll reset
+$.scrollTo(0);
+$("#link1").click(function() { $.scrollTo($("#services"), 500); });
+$("#link2").click(function() { $.scrollTo($("#relax"), 500); });
+$("#link3").click(function() { $.scrollTo($("#project"), 500); });
+$("#link4").click(function() { $.scrollTo($("#clients"), 500); });
+$("#link5").click(function() { $.scrollTo($("#contact"), 500); });
+});
